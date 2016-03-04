a5eb3de09084b65370939fbf83e3f2e0ecf082a9
rfolan <Toyota80>
2016-03-04 08:01:10 -0800

    Adding a simple Profile Page to Alloy.  I will add more to this over time.

---
+++ src/AlloyDemoKit/Controllers/ProfilePageController.cs
@@ -0,0 +1,100 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.Framework.Web;
+using EPiServer.Search;
+using AlloyDemoKit.Business;
+using AlloyDemoKit.Models.ViewModels;
+using AlloyDemoKit.Models.Pages;
+using EPiServer.Web;
+using EPiServer.Web.Hosting;
+using EPiServer.Web.Mvc.Html;
+using EPiServer.Web.Routing;
+using EPiServer.Personalization;
+
+namespace AlloyDemoKit.Controllers
+{
+    public class ProfilePageController : PageControllerBase<ProfilePage>
+    {
+        [HttpPost]
+        [ValidateInput(false)]
+        public ViewResult Save(ProfilePage currentPage, ProfilePageViewModel model2)
+        {
+            model2.Profile.Save();
+
+            var model = new ProfilePageViewModel(currentPage)
+            {
+                Profile = EPiServerProfile.Current,
+                //Impersonating = false
+            };
+
+            //List<string> users = new List<string>();
+
+            //users.Add("Jacob");
+            //users.Add("Rik");
+            //users.Add("Dean");
+
+            //ViewData["users"] = new SelectList(users);
+
+            return View("~/Views/ProfilePage/Index.cshtml", model);
+        }
+
+        public ViewResult Save(ProfilePage currentPage)
+        {
+
+            //model2.Profile.Save();
+
+            var model = new ProfilePageViewModel(currentPage)
+            {
+                Profile = EPiServerProfile.Current,
+                //Impersonating = false
+            };
+            return View("~/Views/ProfilePage/Index.cshtml", model);
+
+        }
+
+        public ViewResult Index(ProfilePage currentPage)
+        {
+
+            var model = new ProfilePageViewModel(currentPage)
+            {
+                Profile = EPiServerProfile.Current,
+                //Impersonating = false
+            };
+
+            //List<string> users = new List<string>();
+
+            //users.Add("Jacob");
+            //users.Add("Rik");
+            //users.Add("Dean");
+
+            //ViewData["users"] = new SelectList(users);
+
+            return View(model);
+        }
+
+        //public ViewResult Impersonate(ProfilePage currentPage, string users)
+        //{
+        //    EPiServerProfile ImpersonatedUser = EPiServerProfile.Get(users);
+
+
+        //    var model = new ProfilePageViewModel(currentPage)
+        //    {
+        //        Profile = ImpersonatedUser,
+        //        //Impersonating = true
+        //    };
+
+        //    List<string> users2 = new List<string>();
+
+        //    users2.Add("Jacob");
+        //    users2.Add("Rik");
+        //    users2.Add("Dean");
+
+        //    ViewData["users"] = new SelectList(users2);
+
+        //    return View("~/Views/ProfilePage/Index.cshtml", model);
+        //}
+
+    }
+}
+
---
+++ src/AlloyDemoKit/Models/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Find.UnifiedSearch;
+using AlloyDemoKit.Models.Pages;
+using EPiServer.Personalization;
+
+namespace AlloyDemoKit.Models.ViewModels
+{
+    public class ProfilePageViewModel : PageViewModel<ProfilePage>
+    {
+        public ProfilePageViewModel() : base()
+        {
+            Profile = EPiServerProfile.Current;
+        }
+
+        public ProfilePageViewModel(ProfilePage currentPage)
+            : base(currentPage)
+        {
+            Profile = EPiServerProfile.Current;
+        }
+
+        public EPiServerProfile Profile { get; set; }
+
+        //public Boolean Impersonating { get; set; }
+    }
+}
