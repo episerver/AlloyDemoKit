fe4b05790c4b387dd9a1b5228e62a8391427ead7
David Knipe <david.knipe@episerver.com>
2016-09-02 13:30:07 +0100

    Add admin mode and vistor groups to quick navigator
    Added admin mode and visitor group links to the quick navigator that
    appears when viewing the site from the front end

---
+++ src/AlloyDemoKit/Business/QuickNavigator/AddAdminMode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.PageExtensions;
+using StructureMap.AutoMocking;
+
+namespace AlloyDemoKit.Business.QuickNavigator
+{
+    [ServiceConfiguration(typeof(IQuickNavigatorItemProvider))]
+    public class AddUsefulLinks : IQuickNavigatorItemProvider
+    {
+        public IDictionary<string, QuickNavigatorMenuItem> GetMenuItems(ContentReference currentContent)
+        {
+            var menuItems = new Dictionary<string, QuickNavigatorMenuItem>();
+
+            if (PrincipalInfo.Current.Principal.IsInRole("CmsAdmins") || PrincipalInfo.Current.Principal.IsInRole("VisitorGroupAdmins"))
+            {
+                menuItems.Add("Visitor Groups",
+                    new QuickNavigatorMenuItem("/shell/cms/visitorgroups/index/name",
+                        UriSupport.ResolveUrlFromUIAsRelativeOrAbsolute("VisitorGroups"), null, "true", null));
+            }
+
+            if (PrincipalInfo.HasAdminAccess)
+            {
+                menuItems.Add("Admin mode",
+                    new QuickNavigatorMenuItem("/shell/cms/menu/admin",
+                        UriSupport.ResolveUrlFromUIAsRelativeOrAbsolute("Admin/default.aspx"), null, "true", null));
+            }
+
+            return menuItems;
+        }
+
+        public int SortOrder => int.MaxValue - 10;
+    }
+}
