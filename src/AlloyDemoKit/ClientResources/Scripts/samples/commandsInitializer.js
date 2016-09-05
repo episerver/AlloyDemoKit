4de74db230e52a292c46af1d8ed8a45921b1beb0
David Knipe <david.knipe@episerver.com>
2016-09-05 11:43:18 +0100

    Display related content in edit mode from publish menu
    Adding the awesome work from here:
    https://gregwiechec.com/2015/06/displaying-related-content-in-edit-mode/

---
+++ src/AlloyDemoKit/ClientResources/Scripts/samples/commandsInitializer.js
@@ -0,0 +1,27 @@
+﻿define([
+    "dojo",
+    "dojo/_base/declare",
+    "epi/_Module",
+    "epi/dependency",
+    "epi/routes",
+    "commands/samples/relatedPagesCommandProvider"
+], function (
+    dojo,
+    declare,
+    _Module,
+    dependency,
+    routes,
+    RelatedPagesCommandProvider
+) {
+    return declare([_Module], {
+        initialize: function () {
+            this.inherited(arguments);
+
+            var relatedPagesCommandProvider = new RelatedPagesCommandProvider();
+
+            var commandregistry = dependency.resolve("epi.globalcommandregistry");
+            var area = "epi.cms.publishmenu";
+            commandregistry.registerProvider(area, relatedPagesCommandProvider);
+        }
+    });
+});
---
+++ src/AlloyDemoKit/ClientResources/Scripts/samples/relatedPagesCommandProvider.js
@@ -0,0 +1,16 @@
+﻿define([
+    "dojo",
+    "dojo/_base/declare",
+    "epi/shell/command/_CommandProviderMixin",
+    "commands/samples/showRelatedPagesCommand"
+], function (dojo, declare, _CommandProviderMixin, ShowRelatedPagesCommand) {
+    return declare([_CommandProviderMixin], {
+
+        constructor: function () {
+            this.inherited(arguments);
+
+            var showRelatedPagesCommand = new ShowRelatedPagesCommand();
+            this.add("commands", showRelatedPagesCommand);
+        }
+    });
+});
