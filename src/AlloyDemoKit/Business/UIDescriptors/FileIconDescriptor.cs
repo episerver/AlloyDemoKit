f0593c876c01eb03173909ffcf28cccdaf020037
davidknipe <knipey@me.com>
2016-10-14 14:06:19 +0100

    Add handling of PDF
    Adds handing of PDF files in the Episerver UI and also adds a renderer
    for content areas. Based on the work of Arve Systad -
    https://www.epinova.no/en/blog/custom-icons-for-content-types/

---
+++ src/AlloyDemoKit/Business/UIDescriptors/FileIconDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using AlloyDemoKit.Models.Media;
+using EPiServer.Core;
+using EPiServer.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyDemoKit.Business.UIDescriptors
+{
+    public class FileIconDescriptor<T> : UIDescriptor<T> where T : ContentData
+    {
+        public FileIconDescriptor()
+        {
+            Type type = GetType();
+            string fileTypeName = type.BaseType.GetGenericArguments()[0].Name;
+            IconClass = fileTypeName.Replace("File", "").ToLower() + "Icon";
+        }
+    }
+
+    [UIDescriptorRegistration]
+    public class PdfFileDescriptor : FileIconDescriptor<PDFFile> { }
+}
---
+++ src/AlloyDemoKit/Models/Media/PDFFile.cs
@@ -0,0 +1,27 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AlloyDemoKit.Models.Media
+{
+    [ContentType(GUID="58341C80-E78F-4F83-AF11-3B48563B41CA")]
+    [MediaDescriptor(ExtensionString = "pdf")]
+    public class PDFFile : MediaData, IFileProperties
+    {
+        ///// <summary>
+        ///// Gets or sets the description.
+        ///// </summary>
+        //public virtual String Title { get; set; }
+
+        [Editable(false)]
+        public virtual string FileSize { get; set; }
+    }
+
+}
+
+
