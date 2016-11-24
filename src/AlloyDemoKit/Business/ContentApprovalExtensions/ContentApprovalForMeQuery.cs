6f74307bc19554a26be9c93737cd922f01c2cc87
davidknipe <knipey@me.com>
2016-11-24 15:53:06 +0000

    Add a task to see content "approvals for me"

---
+++ src/AlloyDemoKit/Business/ContentApprovalExtensions/ContentApprovalForMeQuery.cs
@@ -0,0 +1,39 @@
+﻿using EPiServer.Shell.ContentQuery;
+using System.Collections.Generic;
+using System.Web;
+using EPiServer;
+using EPiServer.Approvals;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell.Rest;
+
+namespace AlloyDemoKit.Business.ContentApprovalExtensions
+{
+    [ServiceConfiguration(typeof(IContentQuery))]
+    public class ContentApprovalForMeQuery : ContentApprovalQueryBase, IContentQuery
+    {
+        public ContentApprovalForMeQuery(IContentRepository contentRepository,
+            IApprovalRepository approvalRepository)
+            : base(contentRepository, approvalRepository)
+        {
+        }
+
+        public QueryRange<IContent> ExecuteQuery(IQueryParameters parameters)
+        {
+            var query = new ApprovalsQuery
+            {
+                OnlyActiveSteps = true,
+                Username = HttpContext.Current.User.Identity.Name
+            };
+            return this.ExecuteQueryInternal(query);
+        }
+
+        public string DisplayName => "Awaiting my approval";
+
+        public string Name => "awaitingmapproval";
+
+        public IEnumerable<string> PlugInAreas => new[] { "editortasks" };
+
+        public int SortOrder => 1;
+    }
+}
