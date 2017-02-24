ce8981588ee5de497d468621a48876e47bcf97b1
davidknipe <knipey@me.com>
2017-02-24 14:50:21 +0000

    Add SiteAttention back in

---
+++ src/AlloyDemoKit/modules/_protected/SiteAttentionModule/ClientResources/Scripts/Content.js
@@ -0,0 +1,1 @@
+﻿define(["dojo", "dojo/_base/declare", "epi/_Module", "epi/dependency", "epi/routes", "epi/shell/_ContextMixin", "siteattention/commandproviders/SiteAttentionCommandProvider"], function (e, t, n, r, i, s, o) { return t("siteattention.Content", [s], { initialize: function () { e.when(this.getCurrentContext(), function (e) { }) } }) })
