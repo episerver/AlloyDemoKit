b94213245a406658b5d12f7aa17c8d7901dd1c9b
davidknipe <knipey@me.com>
2017-03-02 15:38:58 +0000

    Add css overrides for visitor groups to make the style more like the rest of the UI

---
+++ src/AlloyDemoKit/Business/VisitorGroupUIStyling/OverrideVisitorGroupCss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using MenuPin.Attributes;
+
+namespace AlloyDemoKit.Business.VisitorGroupUIStyling
+{
+    public class OverrideVisitorGroupCssAttribute : OutputProcessorActionFilterAttribute
+    {
+        protected override string Process(string data)
+        {
+            return data
+                .Replace("</head", "<link href=\"/Static/css/VisitorGroupUIOverrides.css\" rel=\"stylesheet\"></link></head");
+        }
+
+        protected override bool ShouldProcess(ResultExecutedContext filterContext)
+        {
+            var result = filterContext.Result as ViewResultBase;
+            var view = result?.View as WebFormView;
+            return view != null 
+                && (view.ViewPath.Equals("/EPiServer/CMS/Views/VisitorGroups/Index.aspx") || view.ViewPath.Equals("/EPiServer/CMS/Views/VisitorGroups/Edit.aspx"));
+        }
+    }
+}
---
+++ src/AlloyDemoKit/Business/VisitorGroupUIStyling/OverrideVisitorGroupStylingInit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using MenuPin.Attributes;
+
+namespace AlloyDemoKit.Business.VisitorGroupUIStyling
+{
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class OverrideVisitorGroupStylingInit : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            GlobalFilters.Filters.Add(ServiceLocator.Current.GetInstance<OverrideVisitorGroupCssAttribute>());
+        }
+
+        public void Uninitialize(InitializationEngine context) { }
+    }
+}
