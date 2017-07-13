a3fe770c7794a302cc77ea2e49a9d255a03649f6
David Buö <david.buo@episerver.com>
2017-07-13 16:46:10 +0200

    Fixed bug in DisplayResolutionBase, thanks Marija and Linus.

--- src/AlloyDemoKit/Business/Channels/DisplayResolutionBase.cs
+++ src/AlloyDemoKit/Business/Channels/DisplayResolutionBase.cs
@@ -9,41 +9,54 @@ namespace AlloyDemoKit.Business.Channels
     /// </summary>
     public abstract class DisplayResolutionBase : IDisplayResolution
     {
-        private Injected<LocalizationService> LocalizationService { get; set; }
+        private static readonly LocalizationService _localizationService = ServiceLocator.Current.GetInstance<LocalizationService>();
+        private string _internalName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisplayResolutionBase"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="width">The width in pixels.</param>
+        /// <param name="height">The height in pixels.</param>
         protected DisplayResolutionBase(string name, int width, int height)
         {
-            Id = GetType().FullName;
-            Name = Translate(name);
+            Id = this.GetType().FullName;
+            _internalName = name;
             Width = width;
             Height = height;
         }
 
         /// <summary>
-        /// Gets the unique ID for this resolution
+        /// Gets the unique id for this resolution
         /// </summary>
         public string Id { get; protected set; }
 
         /// <summary>
-        /// Gets the name of resolution
+        /// Gets the name of resolution.
         /// </summary>
-        public string Name { get; protected set; }
+        public string Name
+        {
+            get
+            {
+                return Translate(_internalName);
+            }
+        }
 
         /// <summary>
-        /// Gets the resolution width in pixels
+        /// Gets the resolution width in pixels.
         /// </summary>
         public int Width { get; protected set; }
 
         /// <summary>
-        /// Gets the resolution height in pixels
+        /// Gets the resolution height in pixels.
         /// </summary>
         public int Height { get; protected set; }
 
-        private string Translate(string resurceKey)
+        private static string Translate(string resurceKey)
         {
-            string value;
+            string value = null;
 
-            if(!LocalizationService.Service.TryGetString(resurceKey, out value))
+            if (!_localizationService.TryGetString(resurceKey, out value))
             {
                 value = resurceKey;
             }
